ff6283cfbb6907cd6a60e698d97b6b4bfe3b59cc
Alex Robenko <arobenko@gmail.com>
2018-08-08 20:33:20 +1000

    Disabling some warnings.

--- app/commsdsl2comms/src/IntField.cpp
+++ app/commsdsl2comms/src/IntField.cpp
@@ -684,6 +684,8 @@ void IntField::checkValidRangesOpt(IntField::StringsList& list) const
             auto needToMergeCheck =
                 [](auto min1, auto max1, auto min2, auto max2) -> bool
                 {
+                    static_cast<void>(min1);
+                    static_cast<void>(max2);
                     assert(min1 <= min2);
                     if (min2 <= (max1 + 1)) {
                         assert(max1 <= max2);
