6bff9a3818f1bc17607172aa413ea8efd6c2f27f
Alex Robenko <arobenko@gmail.com>
2018-09-22 13:57:15 +1000

    Generating platform specific headers for all the messages in plugin.

--- app/commsdsl2comms/src/AllMessages.cpp
+++ app/commsdsl2comms/src/AllMessages.cpp
@@ -91,7 +91,10 @@ bool AllMessages::writeProtocolDefinition() const
     }
 
     auto writeFileFunc = 
-        [this](const common::StringsList& incs, const common::StringsList& msgs, const std::string fileName)
+        [this](const common::StringsList& incs, 
+               const common::StringsList& msgs, 
+               const std::string& fileName,
+               const std::string& platName = common::emptyString())
         {
             if (msgs.empty()) {
                 return true;
@@ -120,9 +123,13 @@ bool AllMessages::writeProtocolDefinition() const
             replacements.insert(std::make_pair("MESSAGES", common::listToString(msgs, ",\n", common::emptyString())));
             replacements.insert(std::make_pair("CLASS_NAME", std::move(className)));
 
+            if (!platName.empty()) {
+                replacements.insert(std::make_pair("PLAT_NAME", '\"' + platName + "\" "));
+            }
+
             const std::string Template(
                 "/// @file\n"
-                "/// @brief Contains definition of all messages bundle.\n\n"
+                "/// @brief Contains definition of all #^#PLAT_NAME#$#messages bundle.\n\n"
                 "#pragma once\n\n"
                 "#^#INCLUDES#$#\n"
                 "#^#BEG_NAMESPACE#$#\n"
@@ -156,11 +163,11 @@ bool AllMessages::writeProtocolDefinition() const
         for (auto& p : platformsMap) {
             auto n = common::nameToClassCopy(p.first) + "Messages";
             if (n == common::allMessagesStr()) {
-                m_generator.logger().error("Invalid platforms name: \"" + p.first + "\".");
+                m_generator.logger().error("Invalid platform name: \"" + p.first + "\".");
                 return false;
             }
 
-            if (!writeFileFunc(p.second.m_includes, p.second.m_messages, n)) {
+            if (!writeFileFunc(p.second.m_includes, p.second.m_messages, n, p.first)) {
                 return false;
             }
         }
@@ -169,30 +176,34 @@ bool AllMessages::writeProtocolDefinition() const
 
 bool AllMessages::writePluginDefinition() const
 {
-    auto startInfo = m_generator.startGenericPluginHeaderWrite(common::allMessagesStr());
-    auto& filePath = startInfo.first;
-    auto& className = startInfo.second;
-    
-    if (filePath.empty()) {
-        return true;
-    }
-
-    std::ofstream stream(filePath);
-    if (!stream) {
-        m_generator.logger().error("Failed to open \"" + filePath + "\" for writing.");
-        return false;
-    }
 
     auto allMessages = m_generator.getAllDslMessages();
     common::StringsList messages;
     common::StringsList includes;
     messages.reserve(allMessages.size());
-    includes.reserve(allMessages.size());
+    includes.reserve(allMessages.size() + 1);
+    common::mergeInclude("<tuple>", includes);
 
     std::string interfaceStr;
     auto* interface = m_generator.getDefaultInterface();
     if (interface == nullptr) {
         interfaceStr = "<TInterface>";
+    }    
+
+    struct PlatformInfo
+    {
+        common::StringsList m_messages;
+        common::StringsList m_includes;        
+    };
+
+    using PlatformsMap = std::map<std::string, PlatformInfo>;
+    PlatformsMap platformsMap;
+    auto& platforms = m_generator.platforms();
+    for (auto& p : platforms) {
+        auto& info = platformsMap[p];
+        info.m_messages.reserve(allMessages.size());
+        info.m_includes.reserve(allMessages.size() + 1);
+        common::mergeInclude("<tuple>", info.m_includes);
     }
 
     for (auto m : allMessages) {
@@ -204,44 +215,118 @@ bool AllMessages::writePluginDefinition() const
 
         auto extRef = m.externalRef();
         assert(!extRef.empty());
-        messages.push_back(m_generator.scopeForMessageInPlugin(extRef) + interfaceStr);
-        common::mergeInclude(m_generator.headerfileForMessageInPlugin(extRef, false), includes);
+
+        auto msgStr = m_generator.scopeForMessageInPlugin(extRef) + interfaceStr;
+        auto incStr = m_generator.headerfileForMessageInPlugin(extRef, false);
+        messages.push_back(msgStr);
+        common::mergeInclude(incStr, includes);
+
+        if (platformsMap.empty()) {
+            continue;
+        }
+
+        auto addToPlatformFunc =
+            [&msgStr, &incStr](PlatformInfo& info)
+            {
+                info.m_messages.push_back(msgStr);
+                common::mergeInclude(incStr, info.m_includes);
+            };
+
+        auto& msgPlatforms = m.platforms();
+        if (msgPlatforms.empty()) {
+            for (auto& p : platformsMap) {
+                addToPlatformFunc(p.second);
+            }
+            continue;
+        }
+
+        for (auto& p : msgPlatforms) {
+            auto iter = platformsMap.find(p);
+            if (iter == platformsMap.end()) {
+                assert(!"Should not happen");
+                continue;
+            }
+
+            addToPlatformFunc(iter->second);
+        }
     }
 
-    common::mergeInclude("<tuple>", includes);
+    auto writeFileFunc = 
+        [this, interface](
+                const common::StringsList& incs, 
+                const common::StringsList& msgs, 
+                const std::string& fileName)
+        {
+            if (msgs.empty()) {
+                return true;
+            }
 
-    common::ReplacementMap replacements;
-    auto namespaces = m_generator.namespacesForPlugin();
-    replacements.insert(std::make_pair("BEG_NAMESPACE", std::move(namespaces.first)));
-    replacements.insert(std::make_pair("END_NAMESPACE", std::move(namespaces.second)));
-    replacements.insert(std::make_pair("PROT_NAMESPACE", m_generator.mainNamespace()));
-    replacements.insert(std::make_pair("CLASS_NAME", std::move(className)));
-    replacements.insert(std::make_pair("INCLUDES", common::includesToStatements(includes)));
-    replacements.insert(std::make_pair("MESSAGES", common::listToString(messages, ",\n", common::emptyString())));
 
-    if (interface == nullptr) {
-        replacements.insert(std::make_pair("TEMPLATE_PARAM", "template <typename TInterface>"));
-    }
+            auto startInfo = m_generator.startGenericPluginHeaderWrite(fileName);
+            auto& filePath = startInfo.first;
+            auto& className = startInfo.second;
 
-    const std::string Template(
-        "#pragma once\n\n"
-        "#^#INCLUDES#$#\n"
-        "#^#BEG_NAMESPACE#$#\n"
-        "#^#TEMPLATE_PARAM#$#\n"
-        "using #^#CLASS_NAME#$# =\n"
-        "    std::tuple<\n"
-        "        #^#MESSAGES#$#\n"
-        "    >;\n\n"
-        "#^#END_NAMESPACE#$#\n"
-    );
+            if (filePath.empty()) {
+                return true;
+            }
 
-    auto str = common::processTemplate(Template, replacements);
-    stream << str;
+            std::ofstream stream(filePath);
+            if (!stream) {
+                m_generator.logger().error("Failed to open \"" + filePath + "\" for writing.");
+                return false;
+            }
 
-    stream.flush();
-    if (!stream.good()) {
-        m_generator.logger().error("Failed to write \"" + filePath + "\".");
+            common::ReplacementMap replacements;
+            auto namespaces = m_generator.namespacesForPlugin();
+            replacements.insert(std::make_pair("BEG_NAMESPACE", std::move(namespaces.first)));
+            replacements.insert(std::make_pair("END_NAMESPACE", std::move(namespaces.second)));
+            replacements.insert(std::make_pair("PROT_NAMESPACE", m_generator.mainNamespace()));
+            replacements.insert(std::make_pair("CLASS_NAME", std::move(className)));
+            replacements.insert(std::make_pair("INCLUDES", common::includesToStatements(incs)));
+            replacements.insert(std::make_pair("MESSAGES", common::listToString(msgs, ",\n", common::emptyString())));
+
+            if (interface == nullptr) {
+                replacements.insert(std::make_pair("TEMPLATE_PARAM", "template <typename TInterface>"));
+            }
+
+            const std::string Template(
+                "#pragma once\n\n"
+                "#^#INCLUDES#$#\n"
+                "#^#BEG_NAMESPACE#$#\n"
+                "#^#TEMPLATE_PARAM#$#\n"
+                "using #^#CLASS_NAME#$# =\n"
+                "    std::tuple<\n"
+                "        #^#MESSAGES#$#\n"
+                "    >;\n\n"
+                "#^#END_NAMESPACE#$#\n"
+            );            
+
+            auto str = common::processTemplate(Template, replacements);
+            stream << str;
+
+            stream.flush();
+            if (!stream.good()) {
+                m_generator.logger().error("Failed to write \"" + filePath + "\".");
+                return false;
+            }
+            return true;
+        };
+
+
+    if (!writeFileFunc(includes, messages, common::allMessagesStr())) {
         return false;
+    }
+
+    for (auto& p : platformsMap) {
+        auto n = common::nameToClassCopy(p.first) + "Messages";
+        if (n == common::allMessagesStr()) {
+            m_generator.logger().error("Invalid platform name: \"" + p.first + "\".");
+            return false;
+        }
+
+        if (!writeFileFunc(p.second.m_includes, p.second.m_messages, n)) {
+            return false;
+        }
     }
     return true;
 }
