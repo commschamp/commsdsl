d5aac73609804857a6b87d410c3be09c7f21ce18
Alex Robenko <arobenko@gmail.com>
2019-04-11 16:32:49 +1000

    Updated minimal cc version.

--- app/commsdsl2comms/src/Generator.cpp
+++ app/commsdsl2comms/src/Generator.cpp
@@ -43,7 +43,7 @@ namespace
 {
 
 const unsigned MaxDslVersion = 1U;
-const std::string MinCommsVersionStr("1, 0, 0");
+const std::string MinCommsVersionStr("1, 2, 2");
 const std::string ScopeSep("::");
 const std::string ReplaceSuffix(".replace");
 const std::string ExtendSuffix(".extend");
