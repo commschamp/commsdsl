da52ed0c77f8e35a2ecd54823c463702c60cd7a4
Alex Robenko <arobenko@gmail.com>
2019-05-17 07:29:21 +1000

    Fixing some builds.

--- app/commsdsl2comms/src/SetField.cpp
+++ app/commsdsl2comms/src/SetField.cpp
@@ -706,8 +706,7 @@ void SetField::checkDefaultValueOpt(StringsList& list) const
     }
 
     auto type = obj.type();
-    if ((defaultValue < 0) &&
-        ((type == commsdsl::SetField::Type::Uint64) || (type == commsdsl::SetField::Type::Uintvar))) {
+    if ((type == commsdsl::SetField::Type::Uint64) || (type == commsdsl::SetField::Type::Uintvar)) {
         auto str =
             "comms::option::DefaultBigUnsignedNumValue<" +
             common::numToString(static_cast<std::uintmax_t>(defaultValue), true) +
