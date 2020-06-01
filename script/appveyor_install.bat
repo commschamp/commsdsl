31c6c628854be977d4389c037ac68dd9e5eb7e24
Alex Robenko <arobenko@gmail.com>
2020-06-01 17:42:52 +1000

    Another attempt to fix plugin builds in appveyor.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -3,42 +3,46 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
     set QT_SUBDIR=msvc2015
     set QT_VER=5.6
     set BOOST_VER=1_65_1
+    set ENV_SCRIPT="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2015
-        call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+        set ENV_SCRIPT_PARAM=x86
     ) ELSE (
         echo Performing amd64 build in VS2015
-        call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+        set ENV_SCRIPT_PARAM=amd64
     )
 ) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
     set TOOLCHAIN=msvc15
     set QT_SUBDIR=msvc2017
     set QT_VER=5.11
     set BOOST_VER=1_65_1
+    set ENV_SCRIPT_PARAM=
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2017
-        call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+        set ENV_SCRIPT="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
     ) ELSE (
         echo Performing amd64 build in VS2017
-        call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+        set ENV_SCRIPT="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
     )
-    
 ) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2019" (
     set TOOLCHAIN=msvc16
     set QT_SUBDIR=msvc2017
     set QT_VER=5.14
     set BOOST_VER=1_71_0
+    set ENV_SCRIPT_PARAM=
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2019
-        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
+        set ENV_SCRIPT="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
     ) ELSE (
         echo Performing amd64 build in VS2019
-        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+        set ENV_SCRIPT="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
     )
 ) ELSE (
     echo Toolchain %TOOLCHAIN% is not supported
     exit -1
 )
+
+call "%ENV_SCRIPT%" %ENV_SCRIPT_PARAM%
 
 set QTDIR_PREFIX=C:/Qt/%QT_VER%
 IF "%PLATFORM%"=="x86" (
