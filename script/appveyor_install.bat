fec29cbf37419df27c6e077b0606b24d1e1c0e4e
Alex Robenko <arobenko@gmail.com>
2021-09-11 11:08:18 +1000

    Updating boost version in appveyor configuration.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -28,7 +28,7 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
     set TOOLCHAIN=msvc16
     set QT_SUBDIR=msvc2017
     set QT_VER=5.14
-    set BOOST_VER=1_71_0
+    set BOOST_VER=1_73_0
     set ENV_SCRIPT_PARAM=
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2019
