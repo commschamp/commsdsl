be1f2e4373b0ec8456ea04ac4012f283c19f1c38
Alex Robenko <arobenko@gmail.com>
2022-05-11 08:58:56 +1000

    Making new version to be v4.0.1

--- lib/include/commsdsl/version.h
+++ lib/include/commsdsl/version.h
@@ -26,7 +26,7 @@
 #define COMMSDSL_MINOR_VERSION 0U
 
 /// @brief Patch level of the library
-#define COMMSDSL_PATCH_VERSION 0U
+#define COMMSDSL_PATCH_VERSION 1U
 
 /// @brief Macro to create numeric version as single unsigned number
 #define COMMSDSL_MAKE_VERSION(major_, minor_, patch_) \
