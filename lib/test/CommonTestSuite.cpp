0069c4845392cf9d7333da36c223d79ae71ecf5f
Alex Robenko <arobenko@gmail.com>
2022-07-12 08:15:25 +1000

    Fuxing unreachable code in testing reported by MSVC.

--- lib/test/CommonTestSuite.cpp
+++ lib/test/CommonTestSuite.cpp
@@ -66,24 +66,6 @@ CommonTestSuite::ProtocolPtr CommonTestSuite::prepareProtocol(const std::vector<
             [&protocol](auto& s)
             {
                 return protocol->parse(s);
-                auto dotPos = s.find_last_of('.');
-                auto slashPos = s.find_last_of('/');
-                auto backSlashPos = s.find_last_of('\\');
-
-                if (backSlashPos == std::string::npos) {
-                    backSlashPos = slashPos;
-                }
-
-                if (slashPos == std::string::npos) {
-                    slashPos = backSlashPos;
-                }
-
-                slashPos = std::max(slashPos, backSlashPos);
-                TS_ASSERT_DIFFERS(slashPos, std::string::npos);
-                TS_ASSERT_LESS_THAN(slashPos, dotPos);
-                ++slashPos;
-                auto expSchemaName = s.substr(slashPos, dotPos - slashPos);
-                TS_ASSERT_EQUALS(protocol->lastParsedSchema().name(), expSchemaName);                
             });
 
     TS_ASSERT_EQUALS(parseResult, m_status.m_expParseResult);
@@ -94,5 +76,31 @@ CommonTestSuite::ProtocolPtr CommonTestSuite::prepareProtocol(const std::vector<
 
     bool validateResult = protocol->validate();
     TS_ASSERT_EQUALS(validateResult, m_status.m_expValidateResult);
+
+    auto protSchemas = protocol->schemas();
+    TS_ASSERT_LESS_THAN_EQUALS(protSchemas.size(), schemas.size());
+    for (auto idx = 0U; idx < schemas.size(); ++idx) {
+        auto& s = schemas[idx];
+        auto& protSchema = protSchemas[idx];
+
+        auto dotPos = s.find_last_of('.');
+        auto slashPos = s.find_last_of('/');
+        auto backSlashPos = s.find_last_of('\\');
+
+        if (backSlashPos == std::string::npos) {
+            backSlashPos = slashPos;
+        }
+
+        if (slashPos == std::string::npos) {
+            slashPos = backSlashPos;
+        }
+
+        slashPos = std::max(slashPos, backSlashPos);
+        TS_ASSERT_DIFFERS(slashPos, std::string::npos);
+        TS_ASSERT_LESS_THAN(slashPos, dotPos);
+        ++slashPos;
+        auto expSchemaName = s.substr(slashPos, dotPos - slashPos);
+        TS_ASSERT_EQUALS(protSchema.name(), expSchemaName);     
+    }
     return protocol;
 }
