a221e2c5b211317c037f5c9564305bea3b2aab07
Alex Robenko <arobenko@gmail.com>
2022-09-21 09:15:08 +1000

    Cosmetic update to cmake target names.

--- app/commsdsl2swig/test/commsdsl_lang_python.cmake
+++ app/commsdsl2swig/test/commsdsl_lang_python.cmake
@@ -2,7 +2,7 @@ target_link_libraries(${swig_tgt} Python3::Python)
 
 if (EXISTS ${this_test_dir}/${lang}/swig_${name}.py)
     add_test(
-        NAME ${APP_NAME}.${name}_${lang}_test 
+        NAME ${APP_NAME}.${name}_${lang}
         COMMAND ${Python_EXECUTABLE} ${this_test_dir}/${lang}/swig_${name}.py
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     )
