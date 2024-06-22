bf517a40dd9a2dd4f16d41575b5f3cf6cbeb7e48
Alex Robenko <arobenko@gmail.com>
2024-06-22 16:01:32 +1000

    Using develop branch of the comms library in github actions.

--- app/commsdsl2comms/src/CommsGenerator.cpp
+++ app/commsdsl2comms/src/CommsGenerator.cpp
@@ -68,7 +68,7 @@ namespace util = commsdsl::gen::util;
 namespace commsdsl2comms
 {
 
-const std::string MinCommsVersion("5.2.4");    
+const std::string MinCommsVersion("5.2.5");    
 
 const std::string& CommsGenerator::commsFileGeneratedComment()
 {
