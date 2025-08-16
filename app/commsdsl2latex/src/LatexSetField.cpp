5b63c8268f718792c4cb1710585e4f12938bae92
Alex Robenko <arobenko@gmail.com>
2025-08-16 15:32:54 +1000

    Supporting <set> fields in commsdsl2latex.

--- app/commsdsl2latex/src/LatexSetField.cpp
+++ app/commsdsl2latex/src/LatexSetField.cpp
@@ -17,6 +17,17 @@
 
 #include "LatexGenerator.h"
 
+#include "commsdsl/gen/strings.h"
+#include "commsdsl/gen/util.h"
+
+#include <cassert>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+namespace util = commsdsl::gen::util;
+namespace strings = commsdsl::gen::strings;
+
 namespace commsdsl2latex
 {
 
@@ -31,5 +42,123 @@ bool LatexSetField::genWriteImpl() const
     return latexWrite();
 }
 
+std::string LatexSetField::latexInfoDetailsImpl() const
+{
+    GenStringsList list;
+    auto parseObj = genSetFieldParseObj();
+    
+    list.push_back(latexEndianInfo(parseObj.parseEndian()));
+    return util::genStrListToString(list, "\\\\\\hline\n", "\\\\");
+}
+
+std::string LatexSetField::latexExtraDetailsImpl() const
+{
+    GenStringsList lines;
+    auto parseObj = genSetFieldParseObj();
+    auto& revBits = parseObj.parseRevBits();
+    auto hexWidth = parseObj.parseMinLength() * 2U;
+
+    auto maskStr = 
+        [hexWidth](std::uintmax_t val)
+        {
+            std::stringstream stream;
+            stream << "0x" << std::hex << std::setfill('0') << std::setw(static_cast<int>(hexWidth)) << val;
+            return stream.str();
+        };
+
+    auto idxToMaskStr = 
+        [maskStr](unsigned idx)
+        {
+            return maskStr(1ULL << idx);
+        };
+
+    unsigned gapIdx = 0;
+    for (auto& b : revBits) {
+        while (gapIdx < b.first) {
+            auto l = std::to_string(gapIdx) + " & " + idxToMaskStr(gapIdx) + " & & Reserved. Expected to be " + std::to_string(parseObj.parseReservedBitValue()) + ".";
+            lines.push_back(std::move(l));
+            ++gapIdx;
+            continue;
+        }
+
+        gapIdx = b.first + 1U;
+        auto& bits = parseObj.parseBits();
+        auto iter = bits.find(b.second);
+        assert(iter != bits.end());
+
+        auto& info = iter->second;
+        auto l = std::to_string(b.first) + " & " + idxToMaskStr(b.first) + " & " + 
+            LatexGenerator::latexEscDisplayName(info.m_displayName, b.second) + " & " + 
+            info.m_description;
+
+        auto addVersionInfo = 
+            [&info, &l]()
+            {
+                if (info.m_sinceVersion != 0) {
+                    l.append("Introduced in version " + LatexGenerator::latexIntegralToStr(info.m_sinceVersion) + ". ");
+                }
+
+                if (info.m_deprecatedSince != commsdsl::parse::ParseProtocol::parseNotYetDeprecated()) {
+                    l.append("Deprecated in version " + LatexGenerator::latexIntegralToStr(info.m_deprecatedSince) + ". ");
+                }                
+            };            
+
+        if (!info.m_description.empty()) {
+            if (info.m_description.back() != '.') {
+                l.push_back('.');
+            }
+            
+            l.push_back(' ');
+        }
+
+        if (!info.m_reserved) {
+            addVersionInfo();
+            lines.push_back(std::move(l));
+            continue;
+        }
+
+        l.append("Expected to be " + std::to_string(info.m_reservedValue) + ". ");
+        addVersionInfo();
+        lines.push_back(std::move(l));
+    }
+
+    auto bitLimit = parseObj.parseMinLength() * 8U;
+    if (parseObj.parseBitLength() != 0) {
+        bitLimit = std::min(bitLimit, parseObj.parseBitLength());
+    }
+
+    if (gapIdx < bitLimit) {
+        std::uintmax_t mask = 0U;
+        for (auto i = gapIdx ; i < bitLimit; ++i) {
+            mask |= (1ULL << i);
+        }
+        
+        auto l = std::to_string(gapIdx) + " - " + std::to_string(bitLimit - 1) + " & " +
+            maskStr(mask) + " & & Reserved. Expected to be " + std::to_string(parseObj.parseReservedBitValue()) + ".";
+        lines.push_back(std::move(l));
+    }
+
+    static const std::string Templ = 
+        "\\subsubparagraph{Bit Values}\n"
+        "\\label{#^#LABEL#$#}\n\n"
+        "\\fbox{%\n"
+        "\\begin{tabular}{c|c|c|p{7cm}}\n"
+        "\\textbf{Bit(s) Index} & \\textbf{Bit(s) Mask} & \\textbf{Name} & \\textbf{Description}\\\\\n"
+        "\\hline\n"
+        "\\hline\n"        
+        "#^#LINES#$#\n"
+        "\\end{tabular}\n"
+        "}\n"
+        "\\smallskip\n"
+        "\n"
+        ;
+
+    util::GenReplacementMap repl = {
+        {"LABEL", LatexGenerator::latexLabelId(*this) + "_bits"},
+        {"LINES", util::genStrListToString(lines, " \\\\\\hline\n", " \\\\")},
+    };
+
+    return util::genProcessTemplate(Templ, repl);    
+}
 
 } // namespace commsdsl2latex
