7f5baa616d5c2a3c48d28cd6c82cf73907cd43eb
Alex Robenko <arobenko@gmail.com>
2025-10-30 08:53:11 +1000

    Fixing C++23 builds.

--- script/env_debug_clang.sh
+++ script/env_debug_clang.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-export CC=clang
-export CXX=clang++
+if [ -z "${CC}" ]; then
+    export CC=clang
+fi
+
+if [ -z "${CXX}" ]; then
+    export CXX=clang++
+fi
+
 export COMMON_BUILD_TYPE=Debug
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
