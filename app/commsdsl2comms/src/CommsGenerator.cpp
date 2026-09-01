b9264199e45afa5db055c05f614d2dac9ab82101
Alex Robenko <arobenko@gmail.com>
2026-09-02 08:16:31 +1000

    Requiring comms v5.5.3, cc_tools_qt v6.0.9.

--- app/commsdsl2comms/src/CommsGenerator.cpp
+++ app/commsdsl2comms/src/CommsGenerator.cpp
@@ -67,7 +67,7 @@ namespace util = commsdsl::gen::util;
 namespace commsdsl2comms
 {
 
-const std::string CommsMinCommsVersion("5.5.2");
+const std::string CommsMinCommsVersion("5.5.3");
 
 const std::string& CommsGenerator::commsFileGeneratedComment()
 {
